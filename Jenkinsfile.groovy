0b84c3d5dd5d9f7f82a203d6d7cec44b0ff1c374
svookanti7 <56582216+svookanti7@users.noreply.github.com>
2020-05-08 15:17:55 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,17 @@
-pipeline {
-   agent any
-
-   stages {
-      stage('Hello') {
-         steps {
-            echo 'Hello World'
-         }
-      }
-   }
+pipelineJob('terraform-jenkins-poc') {
+  description('')
+  logRotator(5, 5)
+  parameters {
+   stringParam('gitBranch', 'master', 'Git branch name')
+   choiceParam('terraformenv', ['dev2'], 'Environments where infrastructure needs to be deployed')
+   choiceParam('terraformaccounttype', ['nonprod'], 'The account type the job will run against')  
+   choiceParam('terraformApplyPlan', ['false','true'], 'true: run a terraform plan and apply<br>\
+    false: run a terraform plan only')
+  }
+ definition {
+   cps {
+     script(readFileFromWorkspace('pipelines/Jenkins-terraform-poc'))
+	 sandbox()
+    }
+  }
+} 
